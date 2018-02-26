40d4dad1b39e1b6f1773c2498678dd2fa3d98c1d
thanhvuong1605 <phucthanhvuong@gmail.com>
2018-02-26 03:34:26 +0200

    Initial Commit

---
+++ Order System Mobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Order System Mobile
+//
+//  Created by Thanh Vuong on 2/26/18.
+//  Copyright © 2018 Thanh Vuong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
